b80b555c63f4396b72563cce0eddf39194840849
Thibault Gautriaud <thibault@mixlr.com>
2017-04-20 11:05:00 +0100

    Add default options

--- lib/slack_notifier.rb
+++ lib/slack_notifier.rb
@@ -1,19 +1,20 @@
 require 'slack-notifier'
 
 class SlackNotifier
-  def initialize(webhook_url)
+  def initialize(webhook_url, options = {})
     @webhook_url = webhook_url
+    @options     = options
   end
 
-  def ping(message, options={})
-    options[:username] ||= 'opsbot'
+  def ping(message, options = {})
+    options[:username] ||= 'deploybot'
     options[:icon_emoji] ||= ':robot_face:'
 
     slack.ping(message, options)
   end
 
   private
-  attr_reader :webhook_url
+  attr_reader :webhook_url, :options
 
   def slack
     @slack ||= Slack::Notifier.new(webhook_url)
